1a78f9f3f11244f30cb71fe322c0bb1c19dbfd56
sputnam <seth.putnam@digitalglobe.com>
2018-02-16 13:18:01 -0700

    "Add curl post command"

--- vars/postSlack.groovy
+++ vars/postSlack.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
 
-def call(String path, String pattern, String channel = '#jenkins') {
+def call(String path, String pattern, String channel, String token = '#jenkins') {
     channel = channel ?: '#jenkins'
 
     def imgFiles = new FileNameFinder().getFileNames(path, pattern)
     for (String file : imgFiles) {
         echo file
+        sh "curl -F file=@$file -F channels=$channel -F token=$token https://slack.com/api/files.upload"
     }
 }
